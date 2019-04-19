5b07c89c959e440422d081649534a70e5fd0df0e
boneswvb@gmail.com <boneswvb@gmail.com>
2019-04-19 14:18:04 +0200

    Updates

---
+++ precache-manifest.6662ed069bba39f64dc72272988b244e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends-two/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "482dbb9930e4510bbb67",
+    "url": "/robo-friends-two/static/js/runtime~main.5a8bd222.js"
+  },
+  {
+    "revision": "b6995bc12416e9146bfd",
+    "url": "/robo-friends-two/static/js/main.50ae21f9.chunk.js"
+  },
+  {
+    "revision": "1fbe7b4bf2cf850d4bed",
+    "url": "/robo-friends-two/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "b6995bc12416e9146bfd",
+    "url": "/robo-friends-two/static/css/main.da20d8c6.chunk.css"
+  },
+  {
+    "revision": "1fbe7b4bf2cf850d4bed",
+    "url": "/robo-friends-two/static/css/2.41c1d7d3.chunk.css"
+  },
+  {
+    "revision": "ce8284cd4a2d0fc84c617533a536d39f",
+    "url": "/robo-friends-two/index.html"
+  }
+];
